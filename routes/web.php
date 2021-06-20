1b6bcc591c2d171fee241049c15778559e1cf9f7
Sam Archie <samuelarchie1@gmail.com>
2021-06-20 12:45:17 +0100

    Eager loading by default with protected before

--- routes/web.php
+++ routes/web.php
@@ -35,14 +35,17 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
+    // \Illuminate\Support\Facades\DB::listen(function ($query) {
+    //     logger($query->sql, $query->bindings);
+    // }); An alternative to clockwork
         return view('posts', [
-        'posts' =>  $category->posts
+        'posts' =>  $category->posts->load(['category', 'author'])
     ]);
 });
 
-Route::get('authors/{author}', function (User $author) {
+Route::get('authors/{author:username}', function (User $author) {
         return view('posts', [
-        'posts' =>  $author->posts
+        'posts' =>  $author->posts->load(['category', 'author'])
     ]);
 });
 
