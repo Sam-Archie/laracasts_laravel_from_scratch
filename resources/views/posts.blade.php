f98b5e43df93703a94c52fbe5a8fc3be3a595eca
Sam Archie <samuelarchie1@gmail.com>
2021-06-20 15:08:48 +0100

    blade refacotr this should not be in databases

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,14 @@
 <x-layout>
-    @foreach ($posts as $post)
+    @include('_posts-header')
 
-        <article> {{-- $loop->even?'class-x': '' --}}
-            <h1>
-                <a href="posts/{{ $post->slug }}">
-                    {!! $post->title !!}
-                </a>
-            </h1>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
 
-            <p>
-           By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> In <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
+            <x-post-grid :posts=$posts />
 
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-        
-        </article>
-
-    @endforeach
+        @else
+            <p class="text-center">No posts yet.</p>
+        @endif
+    </main>
 </x-layout>
+
